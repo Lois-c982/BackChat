838457718dcb43cb80f2010755a0500c89ea87bc
Callanshort8 <callanshort8@hotmail.com>
2025-03-30 13:48:13 -0600

    comments Message.java

--- BackChat/src/Message.java
+++ BackChat/src/Message.java
@@ -6,16 +6,19 @@ public class Message implements Serializable {
 	private String recipientID;
 	private String content;
 	private LocalDateTime timestamp;
-	
+
+	//constructor to iniatlise the new message with sender, recipient, content and timestamp
 	public Message(String senderID, String recipientID, String content) {
 		this.senderID = senderID;
 		this.recipientID = recipientID;
 		this.content = content;
 		this.timestamp = LocalDateTime.now();
 	}
-	
+
+	//override to allow the toString method to provide string representation of the message
 	@Override
 	public String toString() {
+		//returns a formatted string showing, sender, recipient, content and timestamp
 		return "From: " + senderID + " to: " + recipientID + "\nMessage: " + content+ "\nTime: " + timestamp;
 	}
 
